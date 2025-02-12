831802551c4f1a4c683d2c2babc7ad587195d29c
mendy770-eng <menachem771@gmail.com>
2025-02-12 16:11:59 +0200

    i created server file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
